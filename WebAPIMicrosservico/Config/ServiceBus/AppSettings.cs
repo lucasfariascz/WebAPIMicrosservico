0673c5d2ed0a1816a1bce1ca3046e9b71df41276
lucasfariascz <lucasfariasdacruz@gmail.com>
2023-11-01 15:04:32 -0300

    Remove Key

--- WebAPIMicrosservico/Config/ServiceBus/AppSettings.cs
+++ WebAPIMicrosservico/Config/ServiceBus/AppSettings.cs
@@ -3,6 +3,6 @@
     public class AppSettings
     {
         public static readonly string QueueName = "webapimicroqueue";
-        public static readonly string AzureServiceBus = "Endpoint=sb://webapimicroqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IDcSIe7o0vdw9qYOBf16LFRz03ST9Mobg+ASbPI3GOc=";
+        public static readonly string AzureServiceBus = "Endpoint=sb://webapiqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=oY9JpImMa+fShpuWLIZGOqwkTVfxoxBxJ+ASbJLpa44=";
     }
 }
